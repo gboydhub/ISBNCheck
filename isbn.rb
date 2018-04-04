4b8d28a573c707fce11615ac510cdbe93413d198
Eponick <mikehuntwife@gmail.com>
2018-04-04 16:38:02 -0400

    Test param is string

--- isbn.rb
+++ isbn.rb
@@ -1,12 +1,16 @@
 require 'minitest/autorun'
 
-def check_isbn(str)
-    return false
-end
-
 class ISBNTest < Minitest::Test
     def test_check_returns_boolean
-            isbnValid = check_isbn("123")
-            assert isbnValid != nil
+        isbnValid = check_isbn("123")
+        assert isbnValid != nil
     end
+
+    def test_isbn_param_is_string
+        assert_kind_of String, InputStr
+    end
+end
+
+def check_isbn(str)
+    return false
 end
